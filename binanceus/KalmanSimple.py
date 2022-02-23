991b8f2e117ff3b06a3a17013c8db2e5d74c78cc
Nateemma <philprice95@gmail.com>
2022-02-22 17:24:18 -0800

    Updated KalmanSimple

--- binanceus/KalmanSimple.py
+++ binanceus/KalmanSimple.py
@@ -50,10 +50,11 @@ This is the 'simple' version, which basically removes all custom sell/stoploss l
 sell signal.
 
 Note that this necessarily requires a 'long' timeframe because predicting a short-term swing is pretty useless - by the
-time a trade was executed, the estimate would be outdated.
-So, I use informative pairs that match the whitelist at 1h intervals to predict movements
+time a trade was executed, the estimate would be outdated. Also, updating the Kalman Filter is expensive and runs
+too slowly if you update every candle.
 
-Custom sell/stoploss logic shamnelessly copied from Solipsis by @werkkrew (https://github.com/werkkrew/freqtrade-strategies)
+So, I use informative pairs that match the whitelist at 1h intervals to predict movements. 
+Results actually seem to be better with the longer timeframe anyway
 
 """
 
@@ -89,18 +90,14 @@ class KalmanSimple(IStrategy):
     startup_candle_count: int = 40
     process_only_new_candles = False
 
-    # Strategy Specific Variable Storage
-    custom_trade_info = {}
-    custom_fiat = "USD"  # Only relevant if stake is BTC or ETH
-    custom_btc_inf = False  # Don't change this.
+    ###################################
 
-    ## Buy Space Hyperopt Variables
+    # Strategy Specific Variable Storage
+
+    ## Hyperopt Variables
 
     # Kalman Filter limits
     buy_kf_gain = DecimalParameter(0.000, 0.050, decimals=3, default=0.015, space='buy', load=True, optimize=True)
-
-    ## Sell Space Params are being used for both custom_stoploss and custom_sell
-
     sell_kf_loss = DecimalParameter(-0.050, 0.000, decimals=3, default=-0.005, space='sell', load=True, optimize=True)
 
     # Kalman Filter
@@ -110,6 +107,8 @@ class KalmanSimple(IStrategy):
                                  initial_state_covariance=1,
                                  observation_covariance=1,
                                  transition_covariance=0.0001)
+
+    ###################################
 
     """
     Informative Pair Definitions
@@ -121,75 +120,23 @@ class KalmanSimple(IStrategy):
         informative_pairs = [(pair, self.inf_timeframe) for pair in pairs]
         return informative_pairs
 
+    ###################################
+
     """
     Indicator Definitions
     """
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # if not metadata['pair'] in self.custom_trade_info:
-        #     self.custom_trade_info[metadata['pair']] = {}
-        #     if not 'had-trend' in self.custom_trade_info[metadata["pair"]]:
-        #         self.custom_trade_info[metadata['pair']]['had-trend'] = False
-        #
-        # ## Base Timeframe / Pair
-        #
-        # # # Kaufmann Adaptive Moving Average
-        # # dataframe['kama'] = ta.KAMA(dataframe, length=233)
-        #
-        # # RMI: https://www.tradingview.com/script/kwIt9OgQ-Relative-Momentum-Index/
-        # dataframe['rmi'] = cta.RMI(dataframe, length=24, mom=5)
-        #
-        # # # Momentum Pinball: https://www.tradingview.com/script/fBpVB1ez-Momentum-Pinball-Indicator/
-        # # dataframe['roc-mp'] = ta.ROC(dataframe, timeperiod=1)
-        # # dataframe['mp'] = ta.RSI(dataframe['roc-mp'], timeperiod=3)
-        #
-        # # MA Streak: https://www.tradingview.com/script/Yq1z7cIv-MA-Streak-Can-Show-When-a-Run-Is-Getting-Long-in-the-Tooth/
-        # dataframe['mastreak'] = cta.mastreak(dataframe, period=4)
-
-        # # Percent Change Channel: https://www.tradingview.com/script/6wwAWXA1-MA-Streak-Change-Channel/
-        # upper, mid, lower = cta.pcc(dataframe, period=40, mult=3)
-        # dataframe['pcc-lowerband'] = lower
-        # dataframe['pcc-upperband'] = upper
-
-        # lookup_idxs = dataframe.index.values - (abs(dataframe['mastreak'].values) + 1)
-        # valid_lookups = lookup_idxs >= 0
-        # dataframe['sbc'] = np.nan
-        # dataframe.loc[valid_lookups, 'sbc'] = dataframe['close'].to_numpy()[lookup_idxs[valid_lookups].astype(int)]
-
-        # dataframe['streak-roc'] = 100 * (dataframe['close'] - dataframe['sbc']) / dataframe['sbc']
-
-        # # Trends, Peaks and Crosses
-        # dataframe['candle-up'] = np.where(dataframe['close'] >= dataframe['open'], 1, 0)
-        # dataframe['candle-up-trend'] = np.where(dataframe['candle-up'].rolling(5).sum() >= 3, 1, 0)
-        #
-        # dataframe['rmi-up'] = np.where(dataframe['rmi'] >= dataframe['rmi'].shift(), 1, 0)
-        # dataframe['rmi-up-trend'] = np.where(dataframe['rmi-up'].rolling(5).sum() >= 3, 1, 0)
-        #
-        # dataframe['rmi-dn'] = np.where(dataframe['rmi'] <= dataframe['rmi'].shift(), 1, 0)
-        # dataframe['rmi-dn-count'] = dataframe['rmi-dn'].rolling(8).sum()
-        #
-        # # dataframe['streak-bo'] = np.where(dataframe['streak-roc'] < dataframe['pcc-lowerband'], 1, 0)
-        # # dataframe['streak-bo-count'] = dataframe['streak-bo'].rolling(8).sum()
-        #
-        # # Indicators used only for ROI and Custom Stoploss
-        # ssldown, sslup = cta.SSLChannels_ATR(dataframe, length=21)
-        # dataframe['sroc'] = cta.SROC(dataframe, roclen=21, emalen=13, smooth=21)
-        # dataframe['ssl-dir'] = np.where(sslup > ssldown, 'up', 'down')
 
         # Base pair informative timeframe indicators
         informative = self.dp.get_pair_dataframe(pair=metadata['pair'], timeframe=self.inf_timeframe)
 
-        # # Get the "average day range" between the 1d high and 1d low to set up guards
-        # informative['1d-high'] = informative['close'].rolling(24).max()
-        # informative['1d-low'] = informative['close'].rolling(24).min()
-        # informative['adr'] = informative['1d-high'] - informative['1d-low']
-
         # Kalman filter
 
-        # update filter (note: this is slow)
+        # update filter (note: this is slow, which is why we run it on the slower timeframe)
         self.kalman_filter = self.kalman_filter.em(informative['close'], n_iter=6)
 
-        # current trend
+        # current trend (if filter.em() is too slow, comment that out and enable the code below. Not as accurate though)
         # mean, cov = self.kalman_filter.filter(informative['close'])
         # informative['kf_mean'] = mean.squeeze()
         # # informative['kf_std'] = np.std(cov.squeeze())
@@ -204,7 +151,15 @@ class KalmanSimple(IStrategy):
 
         dataframe = merge_informative_pair(dataframe, informative, self.timeframe, self.inf_timeframe, ffill=True)
 
+        # copy informative into main timeframe, just to make accessing easier (and to allow further manipulation)
+        dataframe['kf_predict'] = dataframe[f"kf_predict_{self.inf_timeframe}"]
+        dataframe['kf_predict_diff'] = dataframe[f"kf_predict_diff_{self.inf_timeframe}"]
+
+        # NOTE: I played with 'upscaling' the predicted data, but the results were much worse for some reason
+
         return dataframe
+
+    ###################################
 
     """
     Buy Signal
@@ -218,7 +173,7 @@ class KalmanSimple(IStrategy):
 
         # Kalman triggers
         kalman_cond = (
-            qtpylib.crossed_above(dataframe[f"kf_predict_diff_{self.inf_timeframe}"], self.buy_kf_gain.value)
+            qtpylib.crossed_above(dataframe['kf_predict_diff'], self.buy_kf_gain.value)
         )
 
         conditions.append(kalman_cond)
@@ -231,6 +186,8 @@ class KalmanSimple(IStrategy):
 
         return dataframe
 
+    ###################################
+
     """
     Sell Signal
     """
@@ -242,7 +199,7 @@ class KalmanSimple(IStrategy):
 
         # Kalman triggers
         kalman_cond = (
-            qtpylib.crossed_below(dataframe[f"kf_predict_diff_{self.inf_timeframe}"], self.sell_kf_loss.value)
+            qtpylib.crossed_below(dataframe['kf_predict_diff'], self.sell_kf_loss.value)
         )
 
         conditions.append(kalman_cond)
