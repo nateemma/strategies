daad33f034c432773e220e254ae3234f49a019c1
Nateemma <philprice95@gmail.com>
2023-11-27 18:17:59 -0800

    fix compile error

--- TSPredict/TS_Coeff_SWT.py
+++ TSPredict/TS_Coeff_SWT.py
@@ -53,7 +53,7 @@ class TS_Coeff_SWT(TS_Coeff):
         coeffs = pywt.swt(x, wavelet, level=levels)
         
         num_levels = np.shape(coeffs)[0] # varies depending upon the wavelet used
-
+        '''
         # coeff_list = np.array([data[-1]]) # always include the last input data point
         # coeff_list = [data[-1]] # always include the last input data point
         coeff_list = [] 
@@ -70,7 +70,8 @@ class TS_Coeff_SWT(TS_Coeff):
 
         return features
     
-    return self.coeff_to_array(coeffs)
+        '''
+        return self.coeff_to_array(coeffs)
 
     #-------------
 
